8b85b2b19981932c2285d9bcfbc4928bb478cbdb
harshdeep singh <106699976+harshdeep-s@users.noreply.github.com>
2024-05-12 21:23:24 +1000

    Update 6.1p.py

--- 6.1p.py
+++ 6.1p.py
@@ -28,23 +28,28 @@ class LaneDetector:
         rospy.loginfo("image_callback")
 
         # Convert to opencv image 
-        img = self.cv_bridge.compressed_imgmsg_to_cv2(msg, "bgr8") 
-       
-        # Apply color space conversion directly to the cropped image
-        hsv_img = cv2.cvtColor(img[100:500, 100:600], cv2.COLOR_BGR2HSV)   
-        # Create masks for white and yellow colors using cv2.inRange()
-        white_pixel = cv2.inRange(hsv_img, np.array([0, 0, 200])  ,np.array([255, 50, 255]) )
-        yellow_pixel = cv2.inRange(hsv_img, np.array([20, 100, 100]), np.array([40, 255, 255]))
+        img = self.cv_bridge.compressed_imgmsg_to_cv2(msg, "bgr8")
+        
+
+        hsv_img = cv2.cvtColor(img[100:500, 100:600], cv2.COLOR_BGR2HSV) 
+        lower_white = np.array([0, 0, 200])  
+        upper_white = np.array([255, 50, 255])  
+
+        lower_yellow = np.array([20, 100, 100])  
+        upper_yellow = np.array([40, 255, 255])  
+
+        white_pixel = cv2.inRange(hsv_img, lower_white, upper_white)
+        yellow_pixel = cv2.inRange(hsv_img, lower_yellow, upper_yellow)
 
         line_white = self.Hough_lines_apply(cv2.Canny(white_pixel, 50, 150))
         line_yellow = self.Hough_lines_apply(cv2.Canny(yellow_pixel, 50, 150))
 
-        self.detect_line_draw(crop_section, line_white)
-        self.detect_line_draw(crop_section, line_yellow)
+        self.detect_line_draw(img[100:500, 100:600], line_white)
+        self.detect_line_draw(img[100:500, 100:600], line_yellow)
 
         cv2.imshow('White Filter', white_pixel)
         cv2.imshow('Yellow Filter', yellow_pixel)
-        cv2.imshow('Detected Lines', crop_section)
+        cv2.imshow('Detected Lines', img[100:500, 100:600])
         cv2.waitKey(1)
 
     def Hough_lines_apply(self, img):
@@ -59,7 +64,6 @@ class LaneDetector:
                 x1, y1, x2, y2 = line[0]
                 cv2.line(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 i += 1
-                
             
 
     def run(self):
